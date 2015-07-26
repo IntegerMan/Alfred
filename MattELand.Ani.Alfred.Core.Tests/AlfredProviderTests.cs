b31d4037bbaa672084af907897a6d07fbf90f411
Matt Eland <Matt.Eland@Outlook.com>
2015-07-25 23:48:48 -0400

    Added core projects with a basic test and NUnit integration.

---
+++ MattELand.Ani.Alfred.Core.Tests/AlfredProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MattEland.Ani.Alfred.Core;
+using NUnit.Framework;
+
+namespace MattELand.Ani.Alfred.Core.Tests
+{
+    [TestFixture]
+    public class AlfredTests
+    {
+
+        [Test]
+        public void InitializeAlfred()
+        {
+            var alfred = new AlfredProvider();
+        }
+    }
+}
---
+++ MattEland.Ani.Alfred.Core/AlfredProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace MattEland.Ani.Alfred.Core
+{
+    /// <summary>
+    /// Coordinates providing personal assistance to a user interface and receiving settings and queries back from the user interface.
+    /// </summary>
+    public class AlfredProvider
+    {
+    }
+}
