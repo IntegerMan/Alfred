682fa8053396924b1a320042ce6932a2892d47b2
Matt Eland <Matt.Eland@Outlook.com>
2015-07-25 23:59:05 -0400

    WPF now instantiates Alfred.

--- MattEland.Ani.Alfred.WPF/MainWindow.xaml.cs
+++ MattEland.Ani.Alfred.WPF/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using MattEland.Ani.Alfred.Core;
 
 namespace MattEland.Ani.Alfred.WPF
 {
@@ -20,9 +8,16 @@ namespace MattEland.Ani.Alfred.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private AlfredProvider _alfred;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            _alfred = new AlfredProvider();
         }
     }
 }
