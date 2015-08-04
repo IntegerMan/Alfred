e37b8c52b0f663bf3ece7116f54ee9e5e4741ac0
Matt Eland <Matt.Eland@Outlook.com>
2015-08-04 14:53:49 -0400

    Minor cleanup.

--- MattEland.Ani.Alfred.Core.System/SystemMonitorModule.cs
+++ MattEland.Ani.Alfred.Core.System/SystemMonitorModule.cs
@@ -2,7 +2,7 @@
 // SystemMonitorModule.cs
 // 
 // Created on:      08/03/2015 at 8:51 PM
-// Last Modified:   08/04/2015 at 2:37 PM
+// Last Modified:   08/04/2015 at 2:53 PM
 // Original author: Matt Eland
 // ---------------------------------------------------------
 
@@ -18,9 +18,7 @@ using MattEland.Ani.Alfred.Core.Widgets;
 
 namespace MattEland.Ani.Alfred.Core.Modules.SysMonitor
 {
-    /// <summary>
-    ///     A module that displays information on the system
-    /// </summary>
+    /// <summary> A module that displays information on the system </summary>
     public class SystemMonitorModule : AlfredModule
     {
         private const string CpuCategoryName = "Processor";
@@ -36,19 +34,11 @@ namespace MattEland.Ani.Alfred.Core.Modules.SysMonitor
         private readonly List<PerformanceCounter> _processorCounters =
             new List<PerformanceCounter>();
 
-        /// <summary>
-        ///     Initializes a new instance of the
-        ///     <see
-        ///         cref="AlfredModule" />
-        ///     class.
-        /// </summary>
-        /// <param
-        ///     name="platformProvider">
-        ///     The platform provider.
-        /// </param>
-        /// <exception
-        ///     cref="ArgumentNullException">
-        /// </exception>
+        /// <summary> Initializes a new instance of the
+        ///     <see cref="AlfredModule" />
+        ///     class. </summary>
+        /// <param name="platformProvider"> The platform provider. </param>
+        /// <exception cref="ArgumentNullException"> </exception>
         public SystemMonitorModule([NotNull] IPlatformProvider platformProvider) : base(platformProvider)
         {
             var cpuCategory = new PerformanceCounterCategory(CpuCategoryName);
@@ -67,26 +57,20 @@ namespace MattEland.Ani.Alfred.Core.Modules.SysMonitor
             }
         }
 
-        /// <summary>
-        ///     Gets the name of the module.
-        /// </summary>
-        /// <value>The name of the module.</value>
+        /// <summary> Gets the name of the module. </summary>
+        /// <value> The name of the module. </value>
         public override string Name
         {
             get { return "System Monitor"; }
         }
 
-        /// <summary>
-        ///     Handles module shutdown events
-        /// </summary>
+        /// <summary> Handles module shutdown events </summary>
         protected override void ShutdownProtected()
         {
             _cpuWidgets.Clear();
         }
 
-        /// <summary>
-        ///     Handles module initialization events
-        /// </summary>
+        /// <summary> Handles module initialization events </summary>
         protected override void InitializeProtected()
         {
             _cpuWidgets.Clear();
@@ -106,9 +90,7 @@ namespace MattEland.Ani.Alfred.Core.Modules.SysMonitor
             }
         }
 
-        /// <summary>
-        ///     Handles updating the module as needed
-        /// </summary>
+        /// <summary> Handles updating the module as needed </summary>
         protected override void UpdateProtected()
         {
             var cpuIndex = 0;
@@ -120,25 +102,35 @@ namespace MattEland.Ani.Alfred.Core.Modules.SysMonitor
                 cpuIndex++;
 
                 // Display counter value or an error if we somehow didn't have a counter
-                widget.Text = counter != null
-                                  ? $"CPU {cpuIndex}: {GetCpuPercentString(counter)}"
-                                  : $"CPU {cpuIndex}: Error - Not Found";
+                UpdateCpuWidget(widget, counter, cpuIndex.ToString());
             }
-
-            //_processorWidget.IsVisible = true;
-            //_processorWidget.Text = "Processor: " + value;
         }
 
         /// <summary>
-        ///     Gets a processor utilization string as a percentage for a given performance counter, safely handling all documented
-        ///     exceptions
-        ///     for getting the next value.
+        ///     Updates the cpu widget with the next value from its counter or an error message if no counter was provided or
+        ///     an error occurred reading the value.
         /// </summary>
-        /// <param
-        ///     name="counter">
-        ///     The counter.
-        /// </param>
-        /// <returns>System.String.</returns>
+        /// <param name="widget"> The display widget. </param>
+        /// <param name="counter"> The performance counter. </param>
+        /// <param name="instance"> The instance name. </param>
+        private static void UpdateCpuWidget(
+            [NotNull] AlfredTextWidget widget,
+            [CanBeNull] PerformanceCounter counter,
+            [CanBeNull] string instance)
+        {
+            const string NotFoundMessage = "Error - Not Found";
+
+            widget.Text = counter != null
+                              ? $"CPU {instance}: {GetCpuPercentString(counter)}"
+                              : $"CPU {instance}: {NotFoundMessage}";
+        }
+
+        /// <summary>
+        ///     Gets a processor utilization string as a percentage for a given performance counter, safely handling all
+        ///     documented exceptions for getting the next value.
+        /// </summary>
+        /// <param name="counter"> The counter. </param>
+        /// <returns> System.String. </returns>
         [NotNull]
         private static string GetCpuPercentString([NotNull] PerformanceCounter counter)
         {
@@ -147,7 +139,7 @@ namespace MattEland.Ani.Alfred.Core.Modules.SysMonitor
                 throw new ArgumentNullException(nameof(counter));
             }
 
-            try
+            try // Catch each exception thrown by counter.NextValue() instead of catching Exception.
             {
                 // If you try to interpret it as a % in the format string the value will be multiplied * 100.
                 return counter.NextValue().ToString("0.00", CultureInfo.CurrentCulture) + " %";
