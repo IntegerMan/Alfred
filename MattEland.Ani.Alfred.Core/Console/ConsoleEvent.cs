6565b7393469bd356343e70e71f912c368c8fd70
Matt Eland <Matt.Eland@Outlook.com>
2015-08-05 14:56:50 -0400

    Implemented equality members.

--- MattEland.Ani.Alfred.Core/Console/ConsoleEvent.cs
+++ MattEland.Ani.Alfred.Core/Console/ConsoleEvent.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿// ---------------------------------------------------------
+// ConsoleEvent.cs
+// 
+// Created on:      07/26/2015 at 2:23 PM
+// Last Modified:   08/05/2015 at 2:56 PM
+// Original author: Matt Eland
+// ---------------------------------------------------------
+
+using System;
+
+using JetBrains.Annotations;
+
 // ReSharper disable MemberCanBePrivate.Global
 
 namespace MattEland.Ani.Alfred.Core.Console
 {
-    public struct ConsoleEvent
+    /// <summary>
+    ///     Represents a logged event to the console
+    /// </summary>
+    public struct ConsoleEvent : IEquatable<ConsoleEvent>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConsoleEvent"/> class using the current utcTime.
+        ///     Initializes a new instance of the <see cref="ConsoleEvent" /> class using the current utcTime.
         /// </summary>
         /// <param name="title">The title.</param>
         /// <param name="message">The message.</param>
@@ -15,7 +29,7 @@ namespace MattEland.Ani.Alfred.Core.Console
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConsoleEvent"/> class.
+        ///     Initializes a new instance of the <see cref="ConsoleEvent" /> class.
         /// </summary>
         /// <param name="title">The title.</param>
         /// <param name="message">The message.</param>
@@ -28,25 +42,25 @@ namespace MattEland.Ani.Alfred.Core.Console
         }
 
         /// <summary>
-        /// Gets or sets the time the event was logged in UTC.
+        ///     Gets or sets the time the event was logged in UTC.
         /// </summary>
         /// <value>The time the event was logged in UTC.</value>
-        public DateTime UtcTime { get; set; }
+        public DateTime UtcTime { get; }
 
         /// <summary>
-        /// Gets or sets the title.
+        ///     Gets or sets the title.
         /// </summary>
         /// <value>The title.</value>
-        public string Title { get; set; }
+        public string Title { get; }
 
         /// <summary>
-        /// Gets or sets the message.
+        ///     Gets or sets the message.
         /// </summary>
         /// <value>The message.</value>
-        public string Message { get; set; }
+        public string Message { get; }
 
         /// <summary>
-        /// Gets or sets the time in local system time.
+        ///     Gets or sets the time in local system time.
         /// </summary>
         /// <value>The time.</value>
         public DateTime Time
@@ -54,25 +68,35 @@ namespace MattEland.Ani.Alfred.Core.Console
             get { return UtcTime.ToLocalTime(); }
         }
 
-
         #region Equality Members
 
+        /// <summary>
+        ///     Determines if this instance is equivalent to another
+        /// </summary>
+        /// <param name="other">The other event.</param>
+        /// <returns><c>true</c> if the events are equivalent, <c>false</c> otherwise.</returns>
         public bool Equals(ConsoleEvent other)
         {
-            return UtcTime.Equals(other.UtcTime) && string.Equals(Title, other.Title) && string.Equals(Message, other.Message);
+            return UtcTime.Equals(other.UtcTime) && string.Equals(Title, other.Title) &&
+                   string.Equals(Message, other.Message);
         }
 
-        public override bool Equals(object obj)
+        /// <summary>
+        ///     Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current instance.</param>
+        /// <returns><c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.</returns>
+        public override bool Equals([CanBeNull] object obj)
         {
             if (ReferenceEquals(null, obj))
             {
                 return false;
             }
-            return obj is ConsoleEvent && Equals((ConsoleEvent)obj);
+            return obj is ConsoleEvent && Equals((ConsoleEvent) obj);
         }
 
         /// <summary>
-        /// Returns a hash code for this instance.
+        ///     Returns a hash code for this instance.
         /// </summary>
         /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
         public override int GetHashCode()
@@ -80,10 +104,32 @@ namespace MattEland.Ani.Alfred.Core.Console
             unchecked
             {
                 var hashCode = UtcTime.GetHashCode();
-                hashCode = (hashCode * 397) ^ (Title?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Message?.GetHashCode() ?? 0);
+                hashCode = (hashCode*397) ^ (Title?.GetHashCode() ?? 0);
+                hashCode = (hashCode*397) ^ (Message?.GetHashCode() ?? 0);
                 return hashCode;
             }
+        }
+
+        /// <summary>
+        ///     Implements the == operator.
+        /// </summary>
+        /// <param name="left">The left operand.</param>
+        /// <param name="right">The right operand.</param>
+        /// <returns>The result of the operator.</returns>
+        public static bool operator ==(ConsoleEvent left, ConsoleEvent right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        ///     Implements the != operator.
+        /// </summary>
+        /// <param name="left">The left operand.</param>
+        /// <param name="right">The right operand.</param>
+        /// <returns>The result of the operator.</returns>
+        public static bool operator !=(ConsoleEvent left, ConsoleEvent right)
+        {
+            return !left.Equals(right);
         }
 
         #endregion
