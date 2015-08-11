923a07240ce0d8266db8da4cb7a369058c796322
Matt Eland <Matt.Eland@Outlook.com>
2015-08-10 21:34:46 -0400

    Added an option to have the chat system register a page.

--- MattEland.Ani.Alfred.AIML/AlfredChatSubsystem.cs
+++ MattEland.Ani.Alfred.AIML/AlfredChatSubsystem.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿// ---------------------------------------------------------
+// AlfredChatSubsystem.cs
+// 
+// Created on:      08/09/2015 at 11:09 PM
+// Last Modified:   08/10/2015 at 9:33 PM
+// Original author: Matt Eland
+// ---------------------------------------------------------
+
 using System.Collections.Generic;
 using System.Linq;
 
 using JetBrains.Annotations;
 
 using MattEland.Ani.Alfred.Core;
-using MattEland.Ani.Alfred.Core.Console;
 using MattEland.Ani.Alfred.Core.Definitions;
 
 using Res = MattEland.Ani.Alfred.Chat.Resources;
@@ -13,15 +19,15 @@ using Res = MattEland.Ani.Alfred.Chat.Resources;
 namespace MattEland.Ani.Alfred.Chat
 {
     /// <summary>
-    /// The Chat subsystem for Alfred. Presents a chatting mechanism using AIML powered conversation bots.
+    ///     The Chat subsystem for Alfred. Presents a chatting mechanism using AIML powered conversation bots.
     /// </summary>
     public class AlfredChatSubsystem : AlfredSubsystem
     {
         [NotNull]
-        private readonly ChatPage _chatPage;
+        private readonly AimlStatementHandler _chatHandler;
 
         [NotNull]
-        private readonly AimlStatementHandler _chatHandler;
+        private readonly ChatPage _chatPage;
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="AlfredSubsystem" /> class.
@@ -42,7 +48,25 @@ namespace MattEland.Ani.Alfred.Chat
         }
 
         /// <summary>
-        /// Allows components to define controls
+        ///     Gets or sets whether the page should be registered. If not, the chat functionality will just be supplied to the
+        ///     system via the chat handler.
+        /// 
+        ///     Defaults to false.
+        /// </summary>
+        /// <value>Whether or not the page should be registered.</value>
+        public bool RegisterPage { get; set; }
+
+        /// <summary>
+        ///     Gets the name of the component.
+        /// </summary>
+        /// <value>The name of the component.</value>
+        public override string Name
+        {
+            get { return Res.ChatModuleName.NonNull(); }
+        }
+
+        /// <summary>
+        ///     Allows components to define controls
         /// </summary>
         protected override void RegisterControls()
         {
@@ -52,7 +76,10 @@ namespace MattEland.Ani.Alfred.Chat
             _chatHandler.Console = AlfredInstance?.Console;
 
             // Register with the rest of the system
-            //Register(_chatPage);
+            if (RegisterPage)
+            {
+                Register(_chatPage);
+            }
             Register(_chatHandler);
         }
 
@@ -65,16 +92,6 @@ namespace MattEland.Ani.Alfred.Chat
             // Say hi so Alfred greets the user
             _chatHandler.DoInitialGreeting();
         }
-
-        /// <summary>
-        ///     Gets the name of the component.
-        /// </summary>
-        /// <value>The name of the component.</value>
-        public override string Name
-        {
-            get { return Res.ChatModuleName.NonNull(); }
-        }
-
     }
 
 }
