70a95aa836707a846630dd67e5dfaadb09ed1765
Matt Eland <Matt.Eland@Outlook.com>
2015-08-13 15:26:20 -0400

    ALF-13 Added used implicitly to duration.

--- MattEland.Ani.Alfred.Chat.Aiml/Result.cs
+++ MattEland.Ani.Alfred.Chat.Aiml/Result.cs
@@ -66,6 +66,7 @@ namespace MattEland.Ani.Alfred.Chat.Aiml
         ///     Gets the duration of the request.
         /// </summary>
         /// <value>The duration.</value>
+        [UsedImplicitly]
         public TimeSpan Duration { get; private set; }
 
         /// <summary>
