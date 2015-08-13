b402ab794665135a8306463f1214ed75a44c3bd3
Matt Eland <Matt.Eland@Outlook.com>
2015-08-13 00:05:17 -0400

    ALF-13 documented bot gender; corrected settings file.

--- MattEland.Ani.Alfred.Chat.Aiml/Utils/Gender.cs
+++ MattEland.Ani.Alfred.Chat.Aiml/Utils/Gender.cs
@@ -2,17 +2,31 @@
 // Gender.cs
 // 
 // Created on:      08/12/2015 at 10:27 PM
-// Last Modified:   08/12/2015 at 11:59 PM
+// Last Modified:   08/13/2015 at 12:04 AM
 // 
 // Last Modified by: Matt Eland
 // ---------------------------------------------------------
 
 namespace MattEland.Ani.Alfred.Chat.Aiml.Utils
 {
+    /// <summary>
+    ///     An enumeration of bot Genders
+    /// </summary>
     public enum Gender
     {
+        /// <summary>
+        ///     Represents an unknown gender
+        /// </summary>
         Unknown = -1,
+
+        /// <summary>
+        ///     Represents a female bot
+        /// </summary>
         Female = 0,
+
+        /// <summary>
+        ///     Represents a male bot
+        /// </summary>
         Male = 1
     }
 }
