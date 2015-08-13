982a03a1369fa89685909d3321f263049e808c73
Matt Eland <Matt.Eland@Outlook.com>
2015-08-13 14:48:02 -0400

    ALF-13 Document MatchState

--- MattEland.Ani.Alfred.Chat.Aiml/Utils/MatchState.cs
+++ MattEland.Ani.Alfred.Chat.Aiml/Utils/MatchState.cs
@@ -2,17 +2,31 @@
 // MatchState.cs
 // 
 // Created on:      08/12/2015 at 10:27 PM
-// Last Modified:   08/12/2015 at 11:59 PM
+// Last Modified:   08/13/2015 at 2:47 PM
 // 
 // Last Modified by: Matt Eland
 // ---------------------------------------------------------
 
 namespace MattEland.Ani.Alfred.Chat.Aiml.Utils
 {
+    /// <summary>
+    ///     The Match State
+    /// </summary>
     public enum MatchState
     {
+        /// <summary>
+        ///     Represents user input
+        /// </summary>
         UserInput,
+
+        /// <summary>
+        ///     Represents a "That" node
+        /// </summary>
         That,
+
+        /// <summary>
+        ///     Represents an Aiml topic
+        /// </summary>
         Topic
     }
 }
