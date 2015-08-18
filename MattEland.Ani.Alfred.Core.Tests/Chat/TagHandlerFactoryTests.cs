bd2a851ff477a4a3a242d30843840c29aaaffb15
Matt Eland <Matt.Eland@gmail.com>
2015-08-18 17:08:13 -0400

    ALF-27 - Added null constructor test for tag handler factory.

--- MattEland.Ani.Alfred.Core.Tests/Chat/TagHandlerFactoryTests.cs
+++ MattEland.Ani.Alfred.Core.Tests/Chat/TagHandlerFactoryTests.cs
@@ -1,38 +1,30 @@
 ﻿// ---------------------------------------------------------
-// AlfredTagHandlerTests.cs
+// TagHandlerFactoryTests.cs
 // 
-// Created on:      08/17/2015 at 10:55 PM
-// Last Modified:   08/17/2015 at 10:55 PM
+// Created on:      08/18/2015 at 3:59 PM
+// Last Modified:   08/18/2015 at 5:07 PM
 // 
 // Last Modified by: Matt Eland
 // ---------------------------------------------------------
 
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-using System.Xml;
 
-using JetBrains.Annotations;
-
-using MattEland.Ani.Alfred.Chat;
-using MattEland.Ani.Alfred.Chat.Aiml;
 using MattEland.Ani.Alfred.Chat.Aiml.TagHandlers;
-using MattEland.Ani.Alfred.Chat.Aiml.Utils;
-using MattEland.Common;
 
 using NUnit.Framework;
 
 namespace MattEland.Ani.Alfred.Tests.Chat
 {
     /// <summary>
-    /// A class to test <see cref="TagHandlerFactory"/>.
+    ///     A class to test <see cref="TagHandlerFactory" />.
     /// </summary>
     [TestFixture]
     [SuppressMessage("ReSharper", "NotNullMemberIsNotInitialized")]
     public class TagHandlerFactoryTests : ChatTestsBase
     {
         /// <summary>
-        /// Sets up the test fixture for each test.
+        ///     Sets up the test fixture for each test.
         /// </summary>
         [SetUp]
         public void SetUp()
@@ -41,10 +33,45 @@ namespace MattEland.Ani.Alfred.Tests.Chat
         }
 
         /// <summary>
-        /// Parameter validation for TagName
+        ///     When building dynamic and encountering a node that does have a mapping, return a new tag
+        ///     handler
         /// </summary>
         /// <remarks>
-        /// See ALF-29
+        ///     See AlF-29
+        /// </remarks>
+        [Test]
+        public void BuildDynamicWithKnownTagReturnsNewHandler()
+        {
+            var factory = new TagHandlerFactory(Engine);
+            var result = factory.BuildTagHandlerDynamic("srai",
+                                                        BuildTagHandlerParameters(
+                                                                                  "<srai>Testing Rocks</srai>"));
+
+            Assert.IsNotNull(result);
+
+            var handler = (RedirectTagHandler)result;
+            Assert.AreEqual("Testing Rocks", handler.TemplateNode.InnerText);
+        }
+
+        /// <summary>
+        ///     Parameter validation for parameters
+        /// </summary>
+        /// <remarks>
+        ///     See ALF-29
+        /// </remarks>
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BuildDynamicWithNullParamsThrowsException()
+        {
+            var factory = new TagHandlerFactory(Engine);
+            factory.BuildTagHandlerDynamic("Foo", null);
+        }
+
+        /// <summary>
+        ///     Parameter validation for TagName
+        /// </summary>
+        /// <remarks>
+        ///     See ALF-29
         /// </remarks>
         [Test]
         [ExpectedException(typeof(ArgumentNullException))]
@@ -56,52 +83,26 @@ namespace MattEland.Ani.Alfred.Tests.Chat
         }
 
         /// <summary>
-        /// Parameter validation for parameters
+        ///     When building dynamic and encountering a node that doesn't have any mapping, return null
         /// </summary>
         /// <remarks>
-        /// See ALF-29
-        /// </remarks>
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void BuildDynamicWithNullParamsThrowsException()
-        {
-            var factory = new TagHandlerFactory(Engine);
-            factory.BuildTagHandlerDynamic("Foo", null);
-        }
-
-        /// <summary>
-        /// When building dynamic and encountering a node that doesn't have any mapping, return null
-        /// </summary>
-        /// <remarks>
-        /// See AlF-29
+        ///     See AlF-29
         /// </remarks>
         [Test]
         public void BuildDynamicWithUnknownTagReturnsNull()
         {
             var factory = new TagHandlerFactory(Engine);
-            var result = factory.BuildTagHandlerDynamic("Unknown", BuildTagHandlerParameters("<unknown />"));
+            var result = factory.BuildTagHandlerDynamic("Unknown",
+                                                        BuildTagHandlerParameters("<unknown />"));
 
             Assert.IsNull(result);
         }
 
-        /// <summary>
-        /// When building dynamic and encountering a node that does have a mapping, return a new tag handler
-        /// </summary>
-        /// <remarks>
-        /// See AlF-29
-        /// </remarks>
         [Test]
-        public void BuildDynamicWithKnownTagReturnsNewHandler()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BuildFactoryWithNullEngineThrowsException()
         {
-            var factory = new TagHandlerFactory(Engine);
-            var result = factory.BuildTagHandlerDynamic("srai", BuildTagHandlerParameters("<srai>Testing Rocks</srai>"));
-
-            Assert.IsNotNull(result);
-
-            var handler = (RedirectTagHandler)result;
-            Assert.AreEqual("Testing Rocks", handler.TemplateNode.InnerText);
+            var factory = new TagHandlerFactory(null);
         }
-
-
     }
 }
