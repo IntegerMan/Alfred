1304feb58576ab9d2eb26805bfa51b8f6eaaaec7
Matt Eland <Matt.Eland@gmail.com>
2015-08-22 23:36:20 -0400

    ALF-15 - Added test suite for ExplorerPage.

---
+++ MattELand.Ani.Alfred.Core.Tests/Widgets/ExplorerWidgetTests.cs
@@ -0,0 +1,95 @@
+﻿// ---------------------------------------------------------
+// ExplorerWidgetTests.cs
+// 
+// Created on:      08/22/2015 at 11:27 PM
+// Last Modified:   08/22/2015 at 11:27 PM
+// 
+// Last Modified by: Matt Eland
+// ---------------------------------------------------------
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+using JetBrains.Annotations;
+
+using MattEland.Ani.Alfred.Core.Definitions;
+using MattEland.Ani.Alfred.Core.Pages;
+
+using NUnit.Framework;
+
+namespace MattEland.Ani.Alfred.Tests.Widgets
+{
+    /// <summary>
+    /// Contains tests related to the <see cref="ExplorerPage"/>
+    /// </summary>
+    [TestFixture]
+    [SuppressMessage("ReSharper", "NotNullMemberIsNotInitialized")]
+    [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
+    [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+    public class ExplorerWidgetTests
+    {
+        [NotNull]
+        private SimplePlatformProvider _provider;
+        private const string TestPageId = "TestExp";
+        private const string TestPageName = "Explorer Page";
+
+        /// <summary>
+        /// Sets up the environment for each test
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            _provider = new SimplePlatformProvider();
+        }
+
+        /// <summary>
+        /// Simple constructor test for <see cref="ExplorerPage"/>
+        /// </summary>
+        /// <remarks>
+        /// ALF-15
+        /// </remarks>
+        [Test]
+        public void ExplorerPageCanBeCreated()
+        {
+            var page = new ExplorerPage(_provider, TestPageName, TestPageId);
+            Assert.AreEqual(TestPageId, page.Id);
+            Assert.AreEqual(TestPageName, page.Name);
+        }
+
+        /// <summary>
+        /// Simple constructor test for <see cref="ExplorerPage"/> checking parameter validation on provider
+        /// </summary>
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void ExplorerPageThrowsOnNoProvider()
+        {
+            new ExplorerPage(null, TestPageName, TestPageId);
+        }
+        /// <summary>
+        /// Simple constructor test for <see cref="ExplorerPage"/> checking parameter validation on name
+        /// </summary>
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void ExplorerPageThrowsOnNoName()
+        {
+            new ExplorerPage(_provider, null, TestPageId);
+        }
+        /// <summary>
+        /// Simple constructor test for <see cref="ExplorerPage"/> checking parameter validation on id
+        /// </summary>
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void ExplorerPageThrowsOnNoId()
+        {
+            new ExplorerPage(_provider, TestPageName, null);
+        }
+
+        /// <summary>
+        /// Ensures that Explorer pages are visible.
+        /// </summary>
+        [Test]
+        public void ExplorerPageIsVisible()
+        {
+            var page = new ExplorerPage(_provider, TestPageName, TestPageId);
+
+            Assert.That(page.IsVisible);
+        }
+    }
+}
