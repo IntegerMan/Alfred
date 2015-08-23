babf253906cf8ac1502ffc7e0b2c48e6f4d940ac
Matt Eland <Matt.Eland@gmail.com>
2015-08-23 01:10:21 -0400

    ALF-15 - Added barebones XAML structure for Explorer pages and mapping for that control to show up. They're currently empty and extremely white. Lots of work to do on data, capabilities, and styling.

---
+++ MattEland.Ani.Alfred.PresentationShared/Controls/ExplorerControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace MattEland.Ani.Alfred.PresentationShared.Controls
+{
+    /// <summary>
+    /// Interaction logic for ExplorerControl.xaml
+    /// </summary>
+    public partial class ExplorerControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExplorerControl"/> class.
+        /// </summary>
+        public ExplorerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
