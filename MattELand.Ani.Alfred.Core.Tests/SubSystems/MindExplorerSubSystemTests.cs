c2970d4c95425bf4413e951793834cc77e729022
Matt Eland <Matt.Eland@gmail.com>
2015-08-22 23:11:31 -0400

    ALF-15 - Added additional creation / registration tests. Mind Explorer now registers an empty page.

--- MattELand.Ani.Alfred.Core.Tests/SubSystems/MindExplorerSubSystemTests.cs
+++ MattELand.Ani.Alfred.Core.Tests/SubSystems/MindExplorerSubSystemTests.cs
@@ -1,12 +1,19 @@
 ﻿// ---------------------------------------------------------
-// MindExplorerSubSystemTests.cs
+// MindExplorerSubsystemTests.cs
 // 
 // Created on:      08/22/2015 at 10:42 PM
-// Last Modified:   08/22/2015 at 10:42 PM
+// Last Modified:   08/22/2015 at 11:06 PM
 // 
 // Last Modified by: Matt Eland
 // ---------------------------------------------------------
 
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+using JetBrains.Annotations;
+
+using MattEland.Ani.Alfred.Core;
 using MattEland.Ani.Alfred.Core.Definitions;
 using MattEland.Ani.Alfred.Core.SubSystems;
 using MattEland.Common;
@@ -16,29 +23,80 @@ using NUnit.Framework;
 namespace MattEland.Ani.Alfred.Tests.Subsystems
 {
     /// <summary>
-    /// Contains tests related to the MindExplorerSubSystem.
+    ///     Contains tests related to the <see cref="MindExplorerSubsystem" />.
     /// </summary>
+    /// <remarks>
+    ///     See ALF-15
+    /// </remarks>
     [TestFixture]
+    [SuppressMessage("ReSharper", "NotNullMemberIsNotInitialized")]
     public class MindExplorerSubsystemTests
     {
+
         /// <summary>
-        /// Sets up the environment for each test.
+        ///     Sets up the environment for each test.
         /// </summary>
         [SetUp]
         public void SetUp()
         {
+            var bootstrapper = new AlfredBootstrapper();
+            _alfred = bootstrapper.Create();
 
+            _subsystem = new MindExplorerSubsystem(new SimplePlatformProvider());
         }
 
+        [NotNull]
+        private MindExplorerSubsystem _subsystem;
+
+        [NotNull]
+        private AlfredApplication _alfred;
+
         /// <summary>
-        /// Ensures that the mind explorer subsystem can be instantiated
+        ///     Ensures that the mind explorer subsystem can be instantiated
         /// </summary>
+        /// <remarks>
+        ///     See ALF-15
+        /// </remarks>
         [Test]
         public void MindExplorerCanBeInitialized()
         {
-            var subsystem = new MindExplorerSubsystem(new SimplePlatformProvider());
-            Assert.That(subsystem.Name.HasText());
+            Assert.That(_subsystem.Name.HasText());
         }
 
+        /// <summary>
+        ///     Ensures that the Mind Explorer can be registered
+        /// </summary>
+        /// <remarks>
+        ///     See ALF-15
+        /// </remarks>
+        [Test]
+        public void MindExplorerCanBeRegistered()
+        {
+            _alfred.Register(_subsystem);
+
+            Assert.Contains(_subsystem, (ICollection)_alfred.Subsystems);
+        }
+
+        /// <summary>
+        ///     Asserts that the Mind Explorer must register a single Mind Map page.
+        /// </summary>
+        /// <remarks>
+        ///     See ALF-15
+        /// </remarks>
+        [Test]
+        public void MindExplorerHasMindMapPage()
+        {
+            var alfredPages = _alfred.RootPages.Count();
+
+            _alfred.Register(_subsystem);
+
+            Assert.AreEqual(1,
+                            _subsystem.RootPages.Count(),
+                            "Subsystem did not have the expected page count.");
+
+            Assert.AreEqual(alfredPages + 1,
+                            _alfred.RootPages.Count(),
+                            "Alfred did not have the expected page count.");
+        }
     }
 }
--- MattEland.Ani.Alfred.Core/SubSystems/MindExplorerSubSystem.cs
+++ MattEland.Ani.Alfred.Core/SubSystems/MindExplorerSubSystem.cs
@@ -1,8 +1,8 @@
 ﻿// ---------------------------------------------------------
-// MindExplorerSubSystem.cs
+// MindExplorerSubsystem.cs
 // 
 // Created on:      08/22/2015 at 10:48 PM
-// Last Modified:   08/22/2015 at 10:50 PM
+// Last Modified:   08/22/2015 at 11:09 PM
 // 
 // Last Modified by: Matt Eland
 // ---------------------------------------------------------
@@ -11,15 +11,18 @@ using JetBrains.Annotations;
 
 using MattEland.Ani.Alfred.Core.Console;
 using MattEland.Ani.Alfred.Core.Definitions;
+using MattEland.Ani.Alfred.Core.Pages;
 
 namespace MattEland.Ani.Alfred.Core.SubSystems
 {
     /// <summary>
     ///     A SubSystem that pokes around at the internal state of each of Alfred's SubSystems and their
-    ///     sub-components. This SubSystem has a particular focus towards anything AI-releted.
+    ///     sub-components. This SubSystem has a particular focus towards anything AI-related.
     /// </summary>
     public class MindExplorerSubsystem : AlfredSubsystem
     {
+        [NotNull]
+        private readonly AlfredModuleListPage _mindMapPage;
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="AlfredSubsystem" /> class.
@@ -30,6 +33,7 @@ namespace MattEland.Ani.Alfred.Core.SubSystems
         public MindExplorerSubsystem([NotNull] IPlatformProvider provider,
                                      [CanBeNull] IConsole console = null) : base(provider, console)
         {
+            _mindMapPage = new AlfredModuleListPage(provider, "Mind Explorer", "MindMap");
         }
 
         /// <summary>
@@ -48,6 +52,16 @@ namespace MattEland.Ani.Alfred.Core.SubSystems
         public override string Id
         {
             get { return "Mind"; }
+        }
+
+        /// <summary>
+        ///     Allows components to define controls
+        /// </summary>
+        protected override void RegisterControls()
+        {
+            base.RegisterControls();
+
+            Register(_mindMapPage);
         }
     }
 }
