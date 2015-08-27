990d058dd69c7f1c8be175f4f431bafe82822e27
Matt Eland <Matt.Eland@gmail.com>
2015-08-27 15:24:09 -0400

    ALF-98 - IoC container now instantiates using base constructor. Register doesn't work yet, and it's all very weak.

---
+++ MattELand.Ani.Alfred.Core.Tests/Common/CommonProviderTests.cs
@@ -0,0 +1,77 @@
+﻿// ---------------------------------------------------------
+// CommonProviderTests.cs
+// 
+// Created on:      08/27/2015 at 2:49 PM
+// Last Modified:   08/27/2015 at 3:07 PM
+// 
+// Last Modified by: Matt Eland
+// ---------------------------------------------------------
+
+using JetBrains.Annotations;
+
+using MattEland.Common.Providers;
+
+using NUnit.Framework;
+
+namespace MattEland.Ani.Alfred.Tests.Common
+{
+    /// <summary>
+    ///     Tests the Dependency Injection / Inversion of Control container capabilities provided by
+    ///     MattEland.Common
+    /// </summary>
+    [TestFixture]
+    public class CommonProviderTests
+    {
+        /// <summary>
+        ///     A testing class used by <see cref="CommonProviderTests" />
+        /// </summary>
+        public class CommonProviderTestObject
+        {
+            public const string DefaultConstructorUsed = "Default Constructor was Used";
+
+            /// <summary>
+            ///     Initializes a new instance of the <see cref="T:System.Object" /> class.
+            /// </summary>
+            public CommonProviderTestObject() : this(DefaultConstructorUsed)
+            {
+            }
+
+            /// <summary>
+            ///     Initializes a new instance of the <see cref="T:System.Object" /> class.
+            /// </summary>
+            public CommonProviderTestObject([CanBeNull] object data)
+            {
+                Data = data;
+            }
+
+            /// <summary>
+            ///     Gets or sets the data.
+            /// </summary>
+            /// <value>The data.</value>
+            [CanBeNull]
+            public object Data { get; set; }
+        }
+
+        /// <summary>
+        /// Tests that the IoC container provides new instances of the requested type when a type is registered for itself.
+        /// </summary>
+        /// <remarks>
+        /// See ALF-98
+        /// </remarks>
+        [Test]
+        public void RegisterAndCreateInstantiatesUsingDefaultConstructor()
+        {
+            // Tell the container to create types of our object when that type is requested
+            CommonProvider.Register(typeof(CommonProviderTestObject));
+
+            // Use the container to create the instance we want
+            var result = CommonProvider.Create<CommonProviderTestObject>();
+
+            // Check to see that the container created the object using the default constructor
+            Assert.IsNotNull(result, "Item was not instantiated.");
+            Assert.AreEqual(CommonProviderTestObject.DefaultConstructorUsed,
+                            result.Data,
+                            "Default constructor was not used");
+        }
+    }
+}
---
+++ MattEland.Common/Providers/CommonProvider.cs
@@ -0,0 +1,81 @@
+﻿// ---------------------------------------------------------
+// CommonProvider.cs
+// 
+// Created on:      08/27/2015 at 2:55 PM
+// Last Modified:   08/27/2015 at 3:20 PM
+// 
+// Last Modified by: Matt Eland
+// ---------------------------------------------------------
+
+using System;
+
+using JetBrains.Annotations;
+
+namespace MattEland.Common.Providers
+{
+    /// <summary>
+    ///     A dependency injection container used to quickly instantiate classes without coupling ourselves
+    ///     to that implementation.
+    /// </summary>
+    public static class CommonProvider
+    {
+
+        /// <summary>
+        ///     Registers the preferred type as the type to instantiate when the base type is requested.
+        /// </summary>
+        /// <param name="baseType">Type of the base.</param>
+        public static void Register([NotNull] Type baseType) { Register(baseType, baseType); }
+
+        /// <summary>
+        ///     Registers the preferred type as the type to instantiate when the base type is requested.
+        /// </summary>
+        /// <param name="baseType">The type that will be requested.</param>
+        /// <param name="preferredType">
+        ///     The type that should be created when <see cref="baseType" /> is
+        ///     requested.
+        /// </param>
+        public static void Register([NotNull] Type baseType, [NotNull] Type preferredType)
+        {
+            //- Validate
+            if (baseType == null) { throw new ArgumentNullException(nameof(baseType)); }
+            if (preferredType == null) { throw new ArgumentNullException(nameof(preferredType)); }
+
+            // TODO: Register
+        }
+
+        /// <summary>
+        ///     Creates an instance of the requested type.
+        /// </summary>
+        /// <typeparam name="TRequested">The type that was requested to be created.</typeparam>
+        /// <returns>An instance of the requested type</returns>
+        /// <exception cref="TypeInitializationException">The type could not be initialized.</exception>
+        [NotNull]
+        public static TRequested Create<TRequested>()
+        {
+            // Determine which type to create
+            var instantiateType = typeof(TRequested);
+
+            // Create and return an instance of the requested type
+            var instance = CreateInstance(instantiateType);
+
+            return (TRequested)instance;
+        }
+
+        /// <summary>
+        ///     Creates an instance of the requested type.
+        /// </summary>
+        /// <param name="instantiateType">Type to instantiate.</param>
+        /// <returns>A new instance of the requested type.</returns>
+        /// <exception cref="TypeInitializationException">The type could not be initialized.</exception>
+        [NotNull]
+        private static object CreateInstance([NotNull] Type instantiateType)
+        {
+            // Use the activator to create and return an instance of the requested type
+            var instance = Activator.CreateInstance(instantiateType);
+            if (instance != null) { return instance; }
+
+            // Shouldn't be null here (should error out), but just in case throw the exception
+            throw new TypeInitializationException(instantiateType.FullName, null);
+        }
+    }
+}
