ce278c4ef0e655eec3d7b0151150a4428852c76b
Matt Eland <Matt.Eland@gmail.com>
2015-09-01 16:34:40 -0400

    ALF-108 - Removed a usage of Alfred.Console

--- MattEland.Ani.Alfred.AIML/ChatSubsystem.cs
+++ MattEland.Ani.Alfred.AIML/ChatSubsystem.cs
@@ -136,7 +136,7 @@ namespace MattEland.Ani.Alfred.Chat
             base.RegisterControls();
 
             // We don't have console until we're registered, so update it here
-            ChatHandler.Console = AlfredInstance?.Console;
+            ChatHandler.Console = Container.TryProvide<IConsole>();
 
             // Register with the rest of the system
             if (RegisterPage) { Register(_chatPage); }
