d78278e3b12a9f02755672660db2af3bf335c1ef
Matt Eland <Matt.Eland@gmail.com>
2015-09-07 10:22:52 -0400

    Made more classes sealed and added test cases around VisibilityConverter

---
+++ MattELand.Ani.Alfred.Core.Tests/Presentation/VisibilityConverterTests.cs
@@ -0,0 +1,248 @@
+﻿// ---------------------------------------------------------
+// VisibilityConverterTests.cs
+// 
+// Created on:      09/07/2015 at 10:05 AM
+// Last Modified:   09/07/2015 at 10:10 AM
+// 
+// Last Modified by: Matt Eland
+// ---------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Windows;
+
+using JetBrains.Annotations;
+
+using MattEland.Ani.Alfred.PresentationShared.Converters;
+using MattEland.Testing;
+
+using NUnit.Framework;
+
+using Shouldly;
+
+namespace MattEland.Ani.Alfred.Tests.Presentation
+{
+    /// <summary>
+    ///     Provides tests around <see cref="VisibilityConverter" />
+    /// </summary>
+    [UnitTestProvider]
+    public sealed class VisibilityConverterTests : UnitTestBase
+    {
+        /// <summary>
+        ///     The converter.
+        /// </summary>
+        [CanBeNull]
+        private VisibilityConverter _converter;
+
+        /// <summary>
+        ///     Gets the converter.
+        /// </summary>
+        /// <value>
+        /// The converter.
+        /// </value>
+        [NotNull]
+        public VisibilityConverter Converter
+        {
+            get
+            {
+                if (_converter == null) { _converter = new VisibilityConverter(); }
+
+                return _converter;
+            }
+        }
+
+        /// <summary>
+        ///     Gets the target type for conversion.
+        /// </summary>
+        /// <value>
+        ///     The target type.
+        /// </value>
+        private static Type TargetType
+        {
+            get { return typeof(Visibility); }
+        }
+
+        /// <summary>
+        ///     Gets the culture.
+        /// </summary>
+        /// <value>
+        ///     The culture.
+        /// </value>
+        private static CultureInfo Culture
+        {
+            get { return CultureInfo.CurrentCulture; }
+        }
+
+        /// <summary>
+        ///     Tests that passing <see langword="true" /> into the
+        ///     <see cref="VisibilityConverter"/> results in
+        ///     <see cref="System.Windows.Visibility.Visible" /> .
+        /// </summary>
+        [Test]
+        public void TrueResultsInVisible()
+        {
+            //! Arrange
+
+            var converter = Converter;
+
+            //! Act
+
+            var result = converter.Convert(true, TargetType, null, Culture);
+
+            //! Assert
+
+            result.ShouldBe(Visibility.Visible);
+        }
+
+        /// <summary>
+        ///     Tests that passing <see langword="false" /> into the
+        ///     <see cref="VisibilityConverter"/> results in
+        ///     <see cref="System.Windows.Visibility.Collapsed" /> .
+        /// </summary>
+        [Test]
+        public void FalseResultsInCollapsed()
+        {
+            //! Arrange
+
+            var converter = Converter;
+
+            //! Act
+
+            var result = converter.Convert(false, TargetType, null, Culture);
+
+            //! Assert
+
+            result.ShouldBe(Visibility.Collapsed);
+        }
+
+        /// <summary>
+        ///     Tests that passing unexpected inputs into the
+        ///     <see cref="VisibilityConverter"/> results in
+        ///     <see cref="System.Windows.Visibility.Collapsed" /> .
+        /// </summary>
+        [Test]
+        public void ConverterDefaultsToCollapsed()
+        {
+            //! Arrange
+
+            var converter = Converter;
+
+            //! Act
+
+            var result = converter.Convert("MUFASA MUFASA MUFASA!", TargetType, null, Culture);
+
+            //! Assert
+
+            result.ShouldBe(Visibility.Collapsed);
+        }
+
+        /// <summary>
+        ///     Tests that passing a textual representation of <see langword="true"/> into the
+        ///     <see cref="VisibilityConverter" /> results in
+        ///     <see cref="System.Windows.Visibility.Visible" /> .
+        /// </summary>
+        [Test]
+        public void TrueStringResultsInVisible()
+        {
+            //! Arrange
+
+            var converter = Converter;
+
+            //! Act
+
+            var result = converter.Convert("TRUE", TargetType, null, Culture);
+
+            //! Assert
+
+            result.ShouldBe(Visibility.Visible);
+
+        }
+
+        /// <summary>
+        ///     Tests that passing <see langword="true"/> into the
+        ///     <see cref="VisibilityConverter" /> when <see cref="VisibilityConverter.Invert"/> is
+        ///     <see langword="true"/> results in
+        ///     <see cref="System.Windows.Visibility.Collapsed" /> .
+        /// </summary>
+        [Test]
+        public void WhenInvertedTrueReturnsCollapsed()
+        {
+            //! Arrange
+
+            var converter = Converter;
+            converter.Invert = true;
+
+            //! Act
+
+            var result = converter.Convert(true, TargetType, null, Culture);
+
+            //! Assert
+
+            result.ShouldBe(Visibility.Collapsed);
+
+        }
+
+        /// <summary>
+        ///     Tests that passing <see langword="false"/> into the
+        ///     <see cref="VisibilityConverter" /> when <see cref="VisibilityConverter.Invert"/> is
+        ///     <see langword="true"/> results in
+        ///     <see cref="System.Windows.Visibility.Visible" /> .
+        /// </summary>
+        [Test]
+        public void WhenInvertedFalseReturnsVisible()
+        {
+            //! Arrange
+
+            var converter = Converter;
+            converter.Invert = true;
+
+            //! Act
+
+            var result = converter.Convert(false, TargetType, null, Culture);
+
+            //! Assert
+
+            result.ShouldBe(Visibility.Visible);
+        }
+
+        /// <summary>
+        ///     Tests that passing <see langword="null"/> into the
+        ///     <see cref="VisibilityConverter" /> when <see cref="VisibilityConverter.Invert"/> is
+        ///     <see langword="true"/> results in
+        ///     <see cref="System.Windows.Visibility.Visible" /> .
+        /// </summary>
+        [Test]
+        public void NullWhenInvertedReturnsVisible()
+        {
+            //! Arrange
+
+            var converter = Converter;
+            converter.Invert = true;
+
+            //! Act
+
+            var result = converter.Convert(null, TargetType, null, Culture);
+
+            //! Assert
+
+            result.ShouldBe(Visibility.Visible);
+        }
+
+        /// <summary>
+        ///     Tests that <see cref="VisibilityConverter.ConvertBack"/> throws a
+        ///     <see cref="NotSupportedException"/>.
+        /// </summary>
+        [Test, ExpectedException(typeof(NotSupportedException))]
+        public void ConvertBackThrowsNotSupported()
+        {
+            //! Arrange
+
+            var converter = Converter;
+            converter.Invert = true;
+
+            //! Act / Assert (Exception expected)
+
+            var result = converter.ConvertBack(Visibility.Visible, TargetType, null, Culture);
+        }
+    }
+}
--- MattEland.Ani.Alfred.PresentationShared/Converters/VisibilityConverter.cs
+++ MattEland.Ani.Alfred.PresentationShared/Converters/VisibilityConverter.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// ---------------------------------------------------------
+// VisibilityConverter.cs
+// 
+// Created on:      09/03/2015 at 11:00 PM
+// Last Modified:   09/07/2015 at 10:04 AM
+// 
+// Last Modified by: Matt Eland
+// ---------------------------------------------------------
+
+using System;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
@@ -12,59 +17,72 @@ using JetBrains.Annotations;
 namespace MattEland.Ani.Alfred.PresentationShared.Converters
 {
     /// <summary>
-    /// The visibility converter
+    ///     The visibility converter
     /// </summary>
-    public class VisibilityConverter : IValueConverter
+    public sealed class VisibilityConverter : IValueConverter
     {
         /// <summary>
-        /// Gets or sets a value indicating whether the visibility value is inverted so that false is visible and true is not.
+        ///     Gets or sets a value indicating whether the visibility value is inverted so that
+        ///     <see langword="false"/> is visible and <see langword="true"/> is not.
         /// </summary>
-        /// <value><c>true</c> if inverted; otherwise, <c>false</c>.</value>
+        /// <value>
+        /// <c>true</c> if inverted; otherwise, <c>false</c> .
+        /// </value>
+        [UsedImplicitly]
         public bool Invert { get; set; }
 
         /// <summary>
-        /// Converts a value. 
+        ///     Converts a <paramref name="value" /> .
         /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
         /// <returns>
-        /// A converted value. If the method returns null, the valid null value is used.
+        ///     A converted value. If the method returns <see langword="null" /> , the valid
+        ///     <see langword="null" /> <paramref name="value" /> is used.
         /// </returns>
-        /// <param name="value">The value produced by the binding source.</param><param name="targetType">The type of the binding target property.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
-        public object Convert([CanBeNull] object value,
-                              [CanBeNull] Type targetType,
-                              [CanBeNull] object parameter,
-                              [CanBeNull] CultureInfo culture)
+        public object Convert(
+            [CanBeNull] object value,
+            [CanBeNull] Type targetType,
+            [CanBeNull] object parameter,
+            [CanBeNull] CultureInfo culture)
         {
-            bool result = false;
+            var result = false;
 
             if (value != null)
             {
                 // Work with boolean values
                 bool tryBool;
-                if (Boolean.TryParse(value.ToString(), out tryBool))
-                {
-                    result = tryBool;
-                }
+                if (bool.TryParse(value.ToString(), out tryBool)) { result = tryBool; }
             }
 
             // If we're inverting, flip around which output we'll push out
-            if (Invert)
-            {
-                result = !result;
-            }
+            if (Invert) { result = !result; }
 
             // Convert the bool to a visibility
             return result ? Visibility.Visible : Visibility.Collapsed;
         }
 
         /// <summary>
-        /// Converts a value. 
+        ///     Converts a <paramref name="value" /> .
         /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <exception cref="NotSupportedException">
+        /// <see cref="VisibilityConverter.ConvertBack" /> is not supported.
+        /// </exception>
         /// <returns>
-        /// A converted value. If the method returns null, the valid null value is used.
+        ///     A converted value. If the method returns null, the valid <see langword="null" />
+        ///     <paramref name="value" /> is used.
         /// </returns>
-        /// <param name="value">The value that is produced by the binding target.</param><param name="targetType">The type to convert to.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
-        /// <exception cref="NotSupportedException">ConvertBack is not supported</exception>
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(
+            [CanBeNull] object value,
+            [CanBeNull] Type targetType,
+            [CanBeNull] object parameter,
+            [CanBeNull] CultureInfo culture)
         {
             throw new NotSupportedException("ConvertBack is not supported");
         }
