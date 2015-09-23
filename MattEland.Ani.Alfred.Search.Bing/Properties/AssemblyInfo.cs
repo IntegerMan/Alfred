940008bbb210f1838d7ca4ad9ed638eec243cc61
Matt Eland <Matt.Eland@gmail.com>
2015-09-22 21:36:19 -0400

    ALF-84 - Added bing search container. Bing search now is non-portable.

--- MattEland.Ani.Alfred.Search.Bing/Properties/AssemblyInfo.cs
+++ MattEland.Ani.Alfred.Search.Bing/Properties/AssemblyInfo.cs
@@ -1,21 +1,27 @@
 ﻿using System;
-using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MattEland.Ani.Alfred.Search.Bing")]
-[assembly: AssemblyDescription("Bing Search Provider for Alfred")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Matt Eland")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MattEland.Ani.Alfred.Search.Bing")]
-[assembly: AssemblyCopyright("Copyright © Matt Eland 2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en-US")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("383e44c8-1cc9-4793-8e88-f65cad4955cf")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,4 +36,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.3.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
+[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
