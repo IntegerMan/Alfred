d835101cee4b4dd5a096e2ff155204d5840e7792
Matt Eland <Matt.Eland@gmail.com>
2015-10-17 18:40:59 -0400

    Including

---
+++ MattEland.Ani.Alfred.MFDMockUp/Controls/MultifunctionDisplay.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace MattEland.Ani.Alfred.MFDMockUp.Controls
+{
+    /// <summary>
+    /// Represents a cockpit-style multifunction display with buttons bordering a central display.
+    /// </summary>
+    public sealed partial class MultifunctionDisplay : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultifunctionDisplay"/> class.
+        /// </summary>
+        public MultifunctionDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}
