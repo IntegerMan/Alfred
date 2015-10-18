5a14473e33780ae8e698f05bb37875de0eb93d9e
Matt Eland <Matt.Eland@gmail.com>
2015-10-18 14:53:42 -0400

    Improved MFD layout. Buttons don't distribute correctly, though.

--- MattEland.Ani.Alfred.MFDMockUp/Models/MultifunctionDisplay.cs
+++ MattEland.Ani.Alfred.MFDMockUp/Models/MultifunctionDisplay.cs
@@ -3,16 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using Assisticant.Fields;
 
+using MattEland.Common.Annotations;
+
 namespace MattEland.Ani.Alfred.MFDMockUp.Models
 {
     public sealed class MultifunctionDisplay
     {
-        private readonly Observable<string> _name = new Observable<string>();
+        [NotNull]
+        private readonly Observable<string> _name = new Observable<string>("<New MFD>");
 
+        [CanBeNull]
         public string Name
         {
             get { return _name; }
             set { _name.Value = value; }
+        }
+
+        [NotNull]
+        private readonly Observable<double> _screenWidth;
+
+        [NotNull]
+        private readonly Observable<double> _screenHeight;
+
+        public MultifunctionDisplay()
+        {
+            _screenWidth = new Observable<double>(512);
+            _screenHeight = new Observable<double>(512);
+        }
+
+        /// <summary>
+        ///     Gets or sets the width of the screen.
+        /// </summary>
+        /// <value>
+        ///     The width of the screen.
+        /// </value>
+        public double ScreenWidth
+        {
+            get { return _screenWidth; }
+            set { _screenWidth.Value = value; }
+        }
+
+        /// <summary>
+        ///     Gets or sets the height of the screen.
+        /// </summary>
+        /// <value>
+        ///     The height of the screen.
+        /// </value>
+        public double ScreenHeight
+        {
+            get { return _screenHeight; }
+            set { _screenHeight.Value = value; }
         }
     }
 }
