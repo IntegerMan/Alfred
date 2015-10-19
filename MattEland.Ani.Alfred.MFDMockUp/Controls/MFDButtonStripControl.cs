0e8edd000977691160cef5aaf696752345c91e4f
Matt Eland <Matt.Eland@gmail.com>
2015-10-18 22:38:48 -0400

    Button strip is now a Control instead of a CustomControl and now rotates as needed.

---
+++ MattEland.Ani.Alfred.MFDMockUp/Controls/MFDButtonStripControl.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------
+// MFDButtonStripControl.xaml.cs
+// 
+// Created on:      10/17/2015 at 12:43 PM
+// Last Modified:   10/17/2015 at 12:53 PM
+// 
+// Last Modified by: Matt Eland
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+using MattEland.Ani.Alfred.MFDMockUp.Models;
+using MattEland.Common.Annotations;
+
+namespace MattEland.Ani.Alfred.MFDMockUp.Controls
+{
+    /// <summary>
+    /// Represents a strip of buttons bordering a <see cref="MFDControl"/>
+    /// </summary>
+    public sealed class MFDButtonStripControl : Control
+    {
+
+        /// <summary>
+        ///     Defines the <see cref="Orientation"/> dependency property.
+        /// </summary>
+        /// <remarks>
+        ///		Defaults to Horizontal
+        /// </remarks>
+        [NotNull]
+        public static readonly DependencyProperty DockDirectionProperty =
+            DependencyProperty.Register(nameof(DockDirection),
+                                        typeof(ButtonStripDock),
+                                        typeof(MFDButtonStripControl),
+                                        new PropertyMetadata(ButtonStripDock.Top));
+
+        /// <summary>
+        ///     Gets or sets the Dock Direction property using <see cref="DockDirectionProperty"/>.
+        /// </summary>
+        /// <value>The Orientation.</value>
+        public ButtonStripDock DockDirection
+        {
+            get { return (ButtonStripDock)GetValue(DockDirectionProperty); }
+            set { SetValue(DockDirectionProperty, value); }
+        }
+    }
+}
