01d04177ad9bc56d6f6f7bfa098dbbe36c3da591
Matt Eland <Matt.Eland@gmail.com>
2015-10-21 00:08:41 -0400

    Now using custom button base class.

---
+++ MattEland.Ani.Alfred.MFDMockUp/Controls/MFDButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MattEland.Ani.Alfred.MFDMockUp.Controls
+{
+    public sealed class MFDButton : Button
+    {
+        static MFDButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MFDButton), new FrameworkPropertyMetadata(typeof(MFDButton)));
+        }
+    }
+}
