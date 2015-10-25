f04b33ac6bd76dd574e7a1ea663cdee34d93b577
Matt Eland <Matt.Eland@gmail.com>
2015-10-24 23:32:37 -0400

    Used implicitly for code analysis.

--- MattEland.Ani.Alfred.MFDMockUp/Controls/MFDButtonLabelStripControl.xaml.cs
+++ MattEland.Ani.Alfred.MFDMockUp/Controls/MFDButtonLabelStripControl.xaml.cs
@@ -2,11 +2,14 @@
 using System.Linq;
 using System.Windows.Controls;
 
+using MattEland.Common.Annotations;
+
 namespace MattEland.Ani.Alfred.MFDMockUp.Controls
 {
     /// <summary>
     /// Interaction logic for MFDButtonLabelStripControl.xaml
     /// </summary>
+    [UsedImplicitly]
     public sealed partial class MFDButtonLabelStripControl : UserControl
     {
         /// <summary>
