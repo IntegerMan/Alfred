80fee626ccd865b1bc2c5abae4f6e67dac0a91d8
Matt Eland <Matt.Eland@gmail.com>
2015-10-24 23:32:20 -0400

    Sealed.

--- MattEland.Ani.Alfred.MFDMockUp/Controls/MFDButtonLabelStripControl.xaml.cs
+++ MattEland.Ani.Alfred.MFDMockUp/Controls/MFDButtonLabelStripControl.xaml.cs
@@ -7,7 +7,7 @@ namespace MattEland.Ani.Alfred.MFDMockUp.Controls
     /// <summary>
     /// Interaction logic for MFDButtonLabelStripControl.xaml
     /// </summary>
-    public partial class MFDButtonLabelStripControl : UserControl
+    public sealed partial class MFDButtonLabelStripControl : UserControl
     {
         /// <summary>
         ///     Initializes a new instance of the MFDButtonLabelStripControl class.
