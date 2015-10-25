600819c82c61a57405310b6de1ed19e94b54ae23
Matt Eland <Matt.Eland@gmail.com>
2015-10-25 15:47:17 -0400

    Added Alfred screen view model.

---
+++ MattEland.Ani.Alfred.MFDMockUp/Models/Screens/AlfredScreenModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MattEland.Ani.Alfred.MFDMockUp.Models.Screens
+{
+    /// <summary>
+    ///     A data model for the Alfred status screen. This class cannot be inherited.
+    /// </summary>
+    public sealed class AlfredScreenModel : ScreenModel
+    {
+        /// <summary>
+        ///     Initializes a new instance of the AlfredScreenModel class.
+        /// </summary>
+        public AlfredScreenModel() : base("ALF")
+        {
+        }
+
+        /// <summary>
+        ///     Process the screen state and outputs any resulting information to the processorResult.
+        /// </summary>
+        /// <param name="processor"> The processor. </param>
+        /// <param name="processorResult"> The processor result. </param>
+        protected override void ProcessScreenState(MFDProcessor processor, MFDProcessorResult processorResult)
+        {
+            // No operation (yet)
+        }
+    }
+}
---
+++ MattEland.Ani.Alfred.MFDMockUp/ViewModels/Screens/AlfredScreenViewModel.cs
@@ -0,0 +1,72 @@
+using System.Diagnostics.Contracts;
+
+using MattEland.Ani.Alfred.MFDMockUp.Models.Screens;
+using MattEland.Ani.Alfred.PresentationCommon.Helpers;
+using MattEland.Common.Annotations;
+
+namespace MattEland.Ani.Alfred.MFDMockUp.ViewModels.Screens
+{
+    /// <summary>
+    ///     A ViewModel for the Alfred screen.
+    /// </summary>
+    [ViewModelFor(typeof(AlfredScreenModel))]
+    [UsedImplicitly]
+    public sealed class AlfredScreenViewModel
+    {
+        /// <summary>
+        ///     The model.
+        /// </summary>
+        [NotNull]
+        private readonly AlfredScreenModel _model;
+
+        /// <summary>
+        ///     Initializes a new instance of the AlfredScreenViewModel class.
+        /// </summary>
+        /// <param name="model"> The model. </param>
+        public AlfredScreenViewModel([NotNull] AlfredScreenModel model)
+        {
+            _model = model;
+        }
+
+
+        /// <summary>
+        ///     Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="other"> The view model to compare to this instance. </param>
+        /// <returns>
+        ///     true if the specified object  is equal to the current object; otherwise, false.
+        /// </returns>
+        private bool Equals([NotNull] AlfredScreenViewModel other)
+        {
+            Contract.Requires(other != null);
+
+            return _model.Equals(other._model);
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <returns>
+        /// true if the specified object  is equal to the current object; otherwise, false.
+        /// </returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            return obj is AlfredScreenViewModel && Equals((AlfredScreenViewModel)obj);
+        }
+
+        /// <summary>
+        /// Serves as the default hash function. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current object.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return _model.GetHashCode();
+        }
+    }
+}
