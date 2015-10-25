53d9b470f4cb397d22188f67ad71d53deb3ab21f
Matt Eland <Matt.Eland@gmail.com>
2015-10-24 23:31:45 -0400

    Model cleanup.

--- MattEland.Ani.Alfred.MFDMockUp/Controls/MFDButton.cs
+++ MattEland.Ani.Alfred.MFDMockUp/Controls/MFDButton.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace MattEland.Ani.Alfred.MFDMockUp.Controls
 {
+    /// <summary>
+    ///     A multifunction display button. This class cannot be inherited.
+    /// </summary>
     public sealed class MFDButton : Button
     {
+        /// <summary>
+        ///     Initializes static members of the MFDButton class.
+        /// </summary>
         static MFDButton()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(MFDButton), new FrameworkPropertyMetadata(typeof(MFDButton)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MFDButton),
+                new FrameworkPropertyMetadata(typeof(MFDButton)));
         }
     }
 }
