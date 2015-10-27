8ca03fcffabe965c4bb53e99cc4954a964a0d383
Matt Eland <Matt.Eland@gmail.com>
2015-10-26 23:51:05 -0400

    Added a hide if no text converter; hiding button labels when empty.

---
+++ MattEland.Ani.Alfred.PresentationShared/Converters/HideIfNoTextConverter.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------
+// HideIfNoTextConverter.cs
+// 
+// Created on:      10/26/2015 at 11:44 PM
+// Last Modified:   10/26/2015 at 11:44 PM
+// 
+// Last Modified by: Matt Eland
+// ---------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+using MattEland.Common;
+
+namespace MattEland.Ani.Alfred.PresentationAvalon.Converters
+{
+    /// <summary>
+    ///     A value converter that returns Collapsed if null or empty string or Visible otherwise.
+    /// </summary>
+    public sealed class HideIfNoTextConverter : IValueConverter
+    {
+        /// <summary>
+        ///     Converts a value.
+        /// </summary>
+        /// <param name="value"> The value produced by the binding source. </param>
+        /// <param name="targetType"> Type of the target. </param>
+        /// <param name="parameter"> The parameter. </param>
+        /// <param name="culture"> The culture. </param>
+        /// <returns>
+        ///     A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.AsNonNullString().IsEmpty()
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+
+        /// <summary>
+        ///     Converts a value.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        ///     Thrown always as the requested operation is not supported.
+        /// </exception>
+        /// <param name="value"> The value that is produced by the binding target. </param>
+        /// <param name="targetType"> Type of the target. </param>
+        /// <param name="parameter"> The parameter. </param>
+        /// <param name="culture"> The culture. </param>
+        /// <returns>
+        ///     A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
