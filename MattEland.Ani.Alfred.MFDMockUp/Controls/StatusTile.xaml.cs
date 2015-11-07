1c714ddb939d73c98f5dd7052d5a6c9332c5cc57
Matt Eland <Matt.Eland@gmail.com>
2015-11-06 22:36:04 -0500

    Extracted a specialized HeaderedContentControl called StatusTile

---
+++ MattEland.Ani.Alfred.MFDMockUp/Controls/StatusTile.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace MattEland.Ani.Alfred.MFDMockUp.Controls
+{
+    /// <content>
+    ///     The StatusTile is a special type of HeaderedContentControl that presents as a tile in
+    ///     format believable on an MFD screen.
+    /// </content>
+    public partial class StatusTile
+    {
+        /// <summary>
+        ///     Initializes a new instance of the StatusTile class.
+        /// </summary>
+        public StatusTile()
+        {
+            InitializeComponent();
+        }
+    }
+}
