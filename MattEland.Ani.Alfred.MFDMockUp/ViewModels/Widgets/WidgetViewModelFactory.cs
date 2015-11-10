c4158b5c2912e9f53143cb6534375ddd21510285
Matt Eland <Matt.Eland@gmail.com>
2015-11-09 22:02:40 -0500

    Added documentation and notes. Made Widget VM factory interal

--- MattEland.Ani.Alfred.MFDMockUp/ViewModels/Widgets/WidgetViewModelFactory.cs
+++ MattEland.Ani.Alfred.MFDMockUp/ViewModels/Widgets/WidgetViewModelFactory.cs
@@ -14,7 +14,7 @@ namespace MattEland.Ani.Alfred.MFDMockUp.ViewModels.Widgets
     /// <summary>
     ///     A widget view model factory.
     /// </summary>
-    public static class WidgetViewModelFactory
+    internal static class WidgetViewModelFactory
     {
         /// <summary>
         ///     Creates a view model for the specified widget.
@@ -31,12 +31,31 @@ namespace MattEland.Ani.Alfred.MFDMockUp.ViewModels.Widgets
 
             WidgetViewModel output = null;
 
+            // TODO: Refactor this as new widget types are supported
+
             output = TryCreateViewModel<ProgressBarWidget, ProgressBarWidgetViewModel>(widget);
             if (output != null) return output;
 
             return new WidgetViewModel(widget);
         }
 
+        /// <summary>
+        ///     Tries to create a view model for the <paramref name="widget"/> using a specified cast
+        ///     assumption from the two generic type parameters where we assume the widget might be the
+        ///     specified <typeparamref name="TModel" /> and, if it is, we want to instantiate the
+        ///     specified <typeparamref name="TViewModel"/> for it.
+        /// </summary>
+        /// <typeparam name="TModel">
+        ///     Type of the model we suspect <paramref name="widget"/> might be.
+        /// </typeparam>
+        /// <typeparam name="TViewModel">
+        ///     Type of the view model to instantiate if <paramref name="widget"/> is a
+        ///     <typeparamref name="TModel"/>.
+        /// </typeparam>
+        /// <param name="widget"> The widget. </param>
+        /// <returns>
+        ///     A view model of the specified type if the assumption was correct or null otherwise.
+        /// </returns>
         private static TViewModel TryCreateViewModel<TModel, TViewModel>(IWidget widget) where TViewModel : WidgetViewModel
         {
             if (widget is TModel)
